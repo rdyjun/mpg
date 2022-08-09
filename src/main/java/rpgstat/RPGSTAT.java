decb6470e69691f2a9d385d7e2d3250a01da132b
geenee10 <geeenee10@gmail.com>
2022-08-09 23:09:36 +0900

    all debug

--- src/main/java/rpgstat/RPGSTAT.java
+++ src/main/java/rpgstat/RPGSTAT.java
@@ -56,17 +56,16 @@ public class RPGSTAT extends JavaPlugin implements Listener {
         //플레이어 파일 생성
         File pf = new File(getDataFolder(), "playerdata/" + p.getUniqueId() + ".yml");
         FileConfiguration pFile = YamlConfiguration.loadConfiguration(pf);
-        if(p.getExpToLevel() <= e.getAmount()) {
-            UUID pID = p.getUniqueId();
 
-            pFile.set(pID + ".statpoint", pFile.getInt(p.getUniqueId() + "." + "statpoint") + 1);
-            pFile.save(pf);
-            p.sendMessage(ChatColor.BOLD + "" + ChatColor.DARK_GREEN + "[" + ChatColor.GREEN + "STAT" + ChatColor.DARK_GREEN + "] " + ChatColor.RESET + "" + ChatColor.WHITE + "레벨업! 현재 " + p.getLevel() + "레벨 입니다 !");
-            p.sendMessage(ChatColor.BOLD + "" + ChatColor.DARK_GREEN + "[" + ChatColor.GREEN + "STAT" + ChatColor.DARK_GREEN + "] " + ChatColor.RESET + "" + ChatColor.WHITE + "보유 스텟 : " + pFile.get(pID + ".statpoint"));
-        } else {
-            for(String a : pFile.getConfigurationSection("stats").getKeys(false)){
-                pFile.set(p.getUniqueId() + "." + a, 1);
-                pFile.set(p.getUniqueId() + ".statpoint", p.getLevel());
+        p.sendMessage(String.valueOf(e.getAmount()));
+        if(e.getAmount() > 0){
+            if(p.getExp() + e.getAmount() / (float)p.getExpToLevel() >= 1.0) {
+                UUID pID = p.getUniqueId();
+
+                pFile.set(pID + ".statpoint", pFile.getInt(p.getUniqueId() + "." + "statpoint") + 1);
+                pFile.save(pf);
+                p.sendMessage(ChatColor.BOLD + "" + ChatColor.DARK_GREEN + "[" + ChatColor.GREEN + "STAT" + ChatColor.DARK_GREEN + "] " + ChatColor.RESET + "" + ChatColor.WHITE + "레벨업! 현재 " + p.getLevel() + "레벨 입니다 !");
+                p.sendMessage(ChatColor.BOLD + "" + ChatColor.DARK_GREEN + "[" + ChatColor.GREEN + "STAT" + ChatColor.DARK_GREEN + "] " + ChatColor.RESET + "" + ChatColor.WHITE + "보유 스텟 : " + pFile.get(pID + ".statpoint"));
             }
         }
     }
@@ -74,13 +73,14 @@ public class RPGSTAT extends JavaPlugin implements Listener {
     @EventHandler
     public void newPlayer(PlayerJoinEvent e){
         Player p = e.getPlayer();
-//플레이어 파일 생성
+        //플레이어 파일 생성
         File pf = new File(getDataFolder(), "playerdata/" + p.getUniqueId() + ".yml");
         if(!pf.exists()){
             this.playerData = new playerData(this, "playerdata/" + p.getUniqueId() + ".yml");
+            playerData.newPlayer(p);
         }
 
-        playerData.newPlayer(p);
+
     }
     public ItemStack StatInformation(Player p, String ItemName){
         File pf = new File(getDataFolder(),"playerdata/" + p.getUniqueId() + ".yml");
@@ -186,38 +186,88 @@ public class RPGSTAT extends JavaPlugin implements Listener {
                     this.inv(p);
                 } else if(p.isOp()){
                     if(args.length == 1){
-                        if(args[0] == "admin"){
-                            p.sendMessage(ChatColor.BOLD + "" + ChatColor.DARK_GREEN + "[" + ChatColor.GREEN + "STAT" + ChatColor.DARK_GREEN + "]" + ChatColor.RESET + "" + ChatColor.WHITE + "/stat reset [플레이어]" + ChatColor.GREEN + "플레이어의 스텟을 초기화한다. " + ChatColor.GRAY + "(레벨 0)");
-                            p.sendMessage(ChatColor.BOLD + "" + ChatColor.DARK_GREEN + "[" + ChatColor.GREEN + "STAT" + ChatColor.DARK_GREEN + "]" + ChatColor.RESET + "" + ChatColor.WHITE + "/stat set [플레이어] [레벨]" + ChatColor.GREEN + "플레이어의 레벨을 임의로 지정합니다. " + ChatColor.GRAY + "(스텟은 초기화)");
-                        } else if(args[0] == "reset"){
-                            p.sendMessage(ChatColor.BOLD + "" + ChatColor.DARK_GREEN + "[" + ChatColor.GREEN + "STAT" + ChatColor.DARK_GREEN + "]" + ChatColor.RESET + "" + ChatColor.RED + "명령어가 올바르지 않습니다 !");
-                            p.sendMessage(ChatColor.BOLD + "" + ChatColor.DARK_GREEN + "[" + ChatColor.GREEN + "STAT" + ChatColor.DARK_GREEN + "]" + ChatColor.RESET + "" + ChatColor.WHITE + "/stat reset [플레이어]" + ChatColor.GREEN + "플레이어의 스텟을 초기화한다. " + ChatColor.GRAY + "(레벨 0)");
-                        } else if(args[0] == "set"){
-                            p.sendMessage(ChatColor.BOLD + "" + ChatColor.DARK_GREEN + "[" + ChatColor.GREEN + "STAT" + ChatColor.DARK_GREEN + "]" + ChatColor.RESET + "" + ChatColor.RED + "명령어가 올바르지 않습니다 !");
-                            p.sendMessage(ChatColor.BOLD + "" + ChatColor.DARK_GREEN + "[" + ChatColor.GREEN + "STAT" + ChatColor.DARK_GREEN + "]" + ChatColor.RESET + "" + ChatColor.WHITE + "/stat set [플레이어] [레벨]" + ChatColor.GREEN + "플레이어의 레벨을 임의로 지정합니다. " + ChatColor.GRAY + "(스텟은 초기화)");
+                        if(args[0].equalsIgnoreCase("admin")){
+                            p.sendMessage(logo() + ChatColor.WHITE + "/stat reset [플레이어] " + ChatColor.GREEN + "플레이어의 스텟을 초기화한다. " + ChatColor.GRAY + "(레벨 0)");
+                            p.sendMessage(logo() + ChatColor.WHITE + "/stat set [플레이어] [레벨] " + ChatColor.GREEN + "플레이어의 레벨을 임의로 지정합니다. " + ChatColor.GRAY + "(스텟은 초기화)");
+                        } else if(args[0].equalsIgnoreCase("reset")){
+                            p.sendMessage(logo() + ChatColor.RED + "명령어가 올바르지 않습니다 !");
+                            p.sendMessage(logo() + ChatColor.WHITE + "/stat reset [플레이어] " + ChatColor.GREEN + "플레이어의 스텟을 초기화한다. " + ChatColor.GRAY + "(레벨 0)");
+                        } else if(args[0].equalsIgnoreCase("set")){
+                            p.sendMessage(logo() + ChatColor.RED + "명령어가 올바르지 않습니다 !");
+                            p.sendMessage(logo() + ChatColor.WHITE + "/stat set [플레이어] [레벨] " + ChatColor.GREEN + "플레이어의 레벨을 임의로 지정합니다. " + ChatColor.GRAY + "(스텟은 초기화)");
                         }
                     } else if(args.length == 2){
-                        if(args[0] == "reset"){
-                            if(getConfig().contains(args[1])){
-                                Player a = Bukkit.getServer().getPlayer(args[1]);
-                                a.setExp(0);
-                                getConfig().set(a.getUniqueId() + ".statpoint", 0);
-                            } else {
-                                p.sendMessage(ChatColor.BLUE + "[STAT]" + ChatColor.RED + "대상이 올바르지 않습니다 !");
+                        if(args[0].equalsIgnoreCase("reset")){
+                            //플레이어 오류검사
+                            try{
+                                //플레이어 선언
+                                Player a = Bukkit.getServer().getPlayerExact(args[1]);
+                                //플레이어 파일 선언
+                                File pf = new File(getDataFolder(), "playerdata/" + a.getUniqueId() + ".yml");
+                                FileConfiguration pFile = YamlConfiguration.loadConfiguration(pf);
+                                //플레이어 파일 검사
+                                if(pf.exists()){
+                                    a.setLevel(0);
+                                    //스텟 초기화
+                                    for(String b : pFile.getConfigurationSection(String.valueOf(a.getUniqueId())).getKeys(false)){
+                                        pFile.set(a.getUniqueId() + "." + b, 1);
+                                        p.sendMessage(b);
+                                        p.sendMessage(String.valueOf(pFile.get(a.getUniqueId() + "." + b)));
+                                    }
+                                    pFile.set(a.getUniqueId() + ".statpoint", 0);
+                                    p.sendMessage(logo() + ChatColor.WHITE + "[" + a.getName() + "] 님의 레벨(스텟)을 초기화시켰습니다 !");
+                                    //파일 저장
+                                    try {
+                                        pFile.save(pf);
+                                    } catch (IOException e) {
+                                        throw new RuntimeException(e);
+                                    }
+                                } else {
+                                    p.sendMessage(logo() + ChatColor.RED + "대상이 올바르지 않습니다 !");
+                                }
+                            } catch (NullPointerException e){
+                                p.sendMessage(logo() + ChatColor.RED + "대상이 올바르지 않습니다 !");
+                                e.printStackTrace();
+                                return false;
                             }
-                        } else if(args[0] == "set"){
-                            p.sendMessage(ChatColor.BLUE + "[STAT]" + ChatColor.RED + "명령어가 올바르지 않습니다 !");
-                            p.sendMessage(ChatColor.BLUE + "[STAT]" + ChatColor.WHITE + "/stat set [플레이어] [레벨]" + ChatColor.GREEN + "플레이어의 레벨을 임의로 지정합니다. " + ChatColor.GRAY + "(스텟은 초기화)");
+
+                        } else if(args[0].equalsIgnoreCase("set")){
+                            p.sendMessage(logo() + ChatColor.RED + "명령어가 올바르지 않습니다 !");
+                            p.sendMessage(logo() + ChatColor.WHITE + "/stat set [플레이어] [레벨] " + ChatColor.GREEN + "플레이어의 레벨을 임의로 지정합니다. " + ChatColor.GRAY + "(스텟은 초기화)");
                         }
+                    //args.length 가 3개인 경우
                     } else {
-                        if(args[0] == "set"){
-                            if(getConfig().contains(args[1])){
-                                Player a = Bukkit.getServer().getPlayer(args[1]);
-                                a.setLevel(Integer.parseInt(args[3]));
-                                getConfig().set(a.getUniqueId() + ".statpoint", args[3]);
-                            } else {
-                                p.sendMessage(ChatColor.BLUE + "[STAT]" + ChatColor.RED + "대상이 올바르지 않습니다 !");
+                        if(args[0].equalsIgnoreCase("set")){
+                            //플레이어 오류검사
+                            try{
+                                //플레이어 선언
+                                Player a = Bukkit.getServer().getPlayerExact(args[1]);
+                                //파일
+                                File pf = new File(getDataFolder(),"playerdata/" + a.getUniqueId() + ".yml");
+                                FileConfiguration pFile = YamlConfiguration.loadConfiguration(pf);
+                                if(pf.exists()){
+                                    a.setLevel(Integer.parseInt(args[2]));
+                                    //스텟 초기화
+                                    for(String b : pFile.getConfigurationSection(String.valueOf(a.getUniqueId())).getKeys(false)){
+                                        pFile.set(a.getUniqueId() + "." + b, 1);
+                                    }
+                                    //스텟 포인트 설정
+                                    pFile.set(a.getUniqueId() + ".statpoint", Integer.parseInt(args[2]));
+                                    try {
+                                        pFile.save(pf);
+                                    } catch (IOException e) {
+                                        throw new RuntimeException(e);
+                                    }
+                                    p.sendMessage(logo() + ChatColor.WHITE + "[" + a.getName() + "] 님의 스텟이 정상적으로 세팅되었습니다 !");
+                                } else {
+                                    p.sendMessage(logo() + ChatColor.RED + "대상이 올바르지 않습니다 !");
+                                }
+                            } catch (NullPointerException e){
+                                e.printStackTrace();
+                                p.sendMessage(logo() + ChatColor.RED + "대상이 올바르지 않습니다 !");
+                                return false;
                             }
+
                         }
                     }
                 }
@@ -229,7 +279,7 @@ public class RPGSTAT extends JavaPlugin implements Listener {
     public void ivClick(InventoryClickEvent e) throws IOException {
         Player p = (Player) e.getWhoClicked();
         //열려있는(스텟) 인벤토리
-        if(!e.getView().getTitle().contains("스텟")){
+        if(!e.getView().getTitle().contains("스텟") || e.getCurrentItem() == null){
             return;
         }
         //클릭한 아이템에 따른 작동
@@ -240,5 +290,8 @@ public class RPGSTAT extends JavaPlugin implements Listener {
                 p.openInventory(inv(p));
             }
         }
+    }
+    public String logo(){
+        return ChatColor.BOLD + "" + ChatColor.DARK_GREEN + "[" + ChatColor.GREEN + "STAT" + ChatColor.DARK_GREEN + "] " + ChatColor.RESET + "";
     }
 }
